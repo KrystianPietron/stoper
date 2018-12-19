7806a6f3df6c5a0622d0a53b99fd9f2464c3bce3
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-19 10:15:51 +0100

    add store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+// import reducers here
+
+const reducer = combineReducers({
+    // reducers here
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export const store = createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
